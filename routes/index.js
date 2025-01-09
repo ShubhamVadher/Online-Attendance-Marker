dcdcc3c040a7862c4e6cbe2c125a903fed2e08cf
ShubhamVadher <vadhershubhambhai@gmail.com>
2025-01-09 22:41:56 +0530

    Update index.js to have different pages for signup and login

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,27 @@ const router=express.Router();
 
 router.get("/",(req,res)=>{
     const errorsignin=req.query.errorsignin||"";
+    
+    res.render("student-login",{errorsignin});
+    
+})
+
+router.get("/proflogin",(req,res)=>{
+    const errorsignin=req.query.errorsignin||"";
+    
+    res.render("professor-login",{errorsignin});
+})
+
+router.get("/profsignup",(req,res)=>{
+    
     const errorsignup=req.query.errorsignup||"";
-    res.render("index",{errorsignin,errorsignup});
+    res.render("professor-signup",{errorsignup});
+})
+
+router.get("/studentsignup",(req,res)=>{
+    
+    const errorsignup=req.query.errorsignup||"";
+    res.render("student-signup",{errorsignup});
 })
 
 
